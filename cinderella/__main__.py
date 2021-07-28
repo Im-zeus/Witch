0a73cadb5c8999ee1bd70504d91f9acac2c1da78
Im-zeus <87465532+Im-zeus@users.noreply.github.com>
2021-07-28 16:53:10 +0530

    Create __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -27,9 +27,9 @@ from cinderella.modules.connection import connect_button
 
 
 PM_START_TEXT = """
-Hey there! My name is JANVI - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
-
-Join my [support group](https://t.me/darkholdz) to get information on all the latest updates.
+_Hello_ *{}*
+_My name is_ *{}*\n_A Powerful Telegram ProBot to Manage Your Groups,feel free to add to your groups!!_
+_Maintained by_ [{}](tg://user?id={})
 """
 
 
@@ -38,11 +38,11 @@ Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
- 🧚‍♀️ - /start: start the bot
- 🧚‍♀️ - /help: PM's you this message.
- 🧚‍♀️ - /help <module name>: PM's you info about that module.
- 🧚‍♀️ - /source: Information about my source.
- 🧚‍♀️ - /settings:
+ 💠 - /start: start the bot
+ 💠 - /help: PM's you this message.
+ 💠 - /help <module name>: PM's you info about that module.
+ 💠 - /source: Information about my source.
+ 💠 - /settings:
    🔹 - in PM: will send you your settings for all supported modules.
    🔹 - in a group: will redirect you to pm, with all that chat's settings.
 {}
@@ -58,7 +58,8 @@ def vercheck() -> str:
 
 
 SOURCE_STRING = """
-[🧚‍♀️SOURCE🧚‍♀️](https://t.me/neepodamonkey)
+⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/Sur-vivor/CinderellaProBot)
+⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://github.com/Sur-vivor/CinderellaProBot.git)
 """
 
 
@@ -164,7 +165,7 @@ def start(bot: Bot, update: Update, args: List[str]):
             send_start(bot, update)
     else:
         update.effective_message.reply_text("Heya,{} Here..\nHow can I help you? 🙂".format(bot.first_name),reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="🧚‍♀️Help🧚‍♀️",url="t.me/{}?start=help".format(bot.username))]]))
+                                                [[InlineKeyboardButton(text="⚜️Help",url="t.me/{}?start=help".format(bot.username))]]))
 
 def send_start(bot, update):
     #Try to remove old message
@@ -178,8 +179,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="🧚‍♀️𝘏𝘌𝘓𝘗🧚‍♀️",callback_data="help_back"),InlineKeyboardButton(text="🧚‍♀️𝘖𝘞𝘕𝘌𝘙🧚‍♀️",url="https://t.me/darkholdz")]]
-    keyboard += [[InlineKeyboardButton(text="🧚‍♀️𝘊𝘖 𝘖𝘞𝘕𝘌𝘙🧚‍♀️", url=https://t.me/itzmezeus"),InlineKeyboardButton(text="🧚‍♀️𝘈𝘋𝘋 𝘔𝘌🧚‍♀️",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="🛡Creator🛡",url="https://t.me/Surv_ivor")]]
+    keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="⚜️Add Me⚜️",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
@@ -234,7 +235,7 @@ def help_button(bot: Bot, update: Update):
             query.message.reply_text(text=text,
                                      parse_mode=ParseMode.MARKDOWN,
                                      reply_markup=InlineKeyboardMarkup(
-                                         [[InlineKeyboardButton(text="🧚‍♀️Back🧚‍♀️", callback_data="help_back")]]))
+                                         [[InlineKeyboardButton(text="🚶🏻‍♂️Back🚶🏻‍♂️", callback_data="help_back")]]))
 
         elif prev_match:
             curr_page = int(prev_match.group(1))
@@ -279,8 +280,8 @@ def get_help(bot: Bot, update: Update):
 
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="🧚‍♀️Help🧚‍♀️",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="🧚‍♀️Contact Creator🧚‍♀️",url="https://t.me/itzmezeus")]]))
+                                                [[InlineKeyboardButton(text="⚜️Help",url="t.me/{}?start=help".format(bot.username))],  
+                                                [InlineKeyboardButton(text="🛡Contact Creator",url="https://t.me/Surv_ivor")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
@@ -342,7 +343,7 @@ def settings_button(bot: Bot, update: Update):
             query.message.reply_text(text=text,
                                      parse_mode=ParseMode.MARKDOWN,
                                      reply_markup=InlineKeyboardMarkup(
-                                         [[InlineKeyboardButton(text="🧚‍♀️Back🧚‍♀️",
+                                         [[InlineKeyboardButton(text="🏃🏻‍♂️Back🏃🏻‍♂️",
                                                                 callback_data="stngs_back({})".format(chat_id))]]))
 
         elif prev_match:
@@ -401,7 +402,7 @@ def get_settings(bot: Bot, update: Update):
             text = "Click here to get this chat's settings, as well as yours."
             msg.reply_text(text,
                            reply_markup=InlineKeyboardMarkup(
-                               [[InlineKeyboardButton(text="🧚‍♀️Settings🧚‍♀️",
+                               [[InlineKeyboardButton(text="⚙️Settings⚙️",
                                                       url="t.me/{}?start=stngs_{}".format(
                                                           bot.username, chat.id))]]))
         else:
